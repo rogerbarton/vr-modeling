4501a672e3afc084426ea170ae55c243d001c7db
Roger Barton <rbarton@student.ethz.ch>
2020-03-11 19:04:13 +0100

    debug mesh script
    Set a breakpoint to see vertex buffer layout of a mesh

---
+++ Assets/Scripts/DebugMesh.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class DebugMesh : MonoBehaviour
+{
+    private Mesh mesh;
+    public VertexAttributeDescriptor[] layout;
+    private void Start()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+        layout = mesh.GetVertexAttributes();
+        var length = layout.Length;
+        
+    }
+}
