96af79c37cd95a1288a5c5a689bc56fc3b0fca81
Roger Barton <rbarton@student.ethz.ch>
2020-03-16 21:42:55 +0100

    Add UnityNativeTool stubLluiPlugin to cmake so UnityLoadPlugin works
    UploadMesh now works partially, still a crash at the end of play mode.

---
+++ Interface/external/UnityNativeTool/stubLluiPlugin.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include "IUnityInterface.h"
+
+// A one file library that gets us the pointer to the UnityInterfaces
+// This is used by the UnityNativeTool to call UnityPluginLoad and
+// UnityPluginUnload for our mocked libraries. This library is not mocked.
+
+static IUnityInterfaces* s_IUnityInterfaces = NULL;
+
+// Get the UnityInterfaces pointer as with a normal plugin, called by Unity
+UNITY_INTERFACE_EXPORT void UNITY_INTERFACE_API UnityPluginLoad(IUnityInterfaces* unityInterfaces)
+{
+	s_IUnityInterfaces = unityInterfaces;
+}
+
+// Allow us to retrieve this pointer from C#
+UNITY_INTERFACE_EXPORT IUnityInterfaces* UNITY_INTERFACE_API GetUnityInterfacesPtr()
+{
+	return s_IUnityInterfaces;
+}
