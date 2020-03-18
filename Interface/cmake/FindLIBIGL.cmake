4cf044778e04a110d6a5842bcc5ebf6bc3352418
Roger Barton <rbarton@student.ethz.ch>
2020-03-18 19:27:55 +0100

    Fix FindLibigl.cmake
    look in the external folder as well

--- Interface/cmake/FindLIBIGL.cmake
+++ Interface/cmake/FindLIBIGL.cmake
@@ -20,6 +20,7 @@ find_path(LIBIGL_INCLUDE_DIR igl/readOBJ.h
         ${CMAKE_SOURCE_DIR}/libigl
         ${CMAKE_SOURCE_DIR}/../libigl
         ${CMAKE_SOURCE_DIR}/../../libigl
+        ${CMAKE_SOURCE_DIR}/external/libigl
         /usr
         /usr/local
         /usr/local/igl/libigl
