f3950e6b8ed4e9d49733713346dd41fc7969f680
Roger Barton <rbarton@student.ethz.ch>
2020-03-30 13:42:59 +0200

    Basic speech-to-text working on windows

---
+++ Assets/Scripts/SpeechTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class SpeechTest : MonoBehaviour
+{
+    private KeywordRecognizer _keywordRecognizer;
+    private Dictionary<string, Action> _actions = new Dictionary<string, Action>();
+    
+    public void Hello()
+    {
+        Debug.Log("Hello");
+    }
+
+    public void OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        Debug.Log($"OnPhraseRecognized {args.text}, confidence {args.confidence}");
+        _actions[args.text].Invoke();
+    }
+
+    void Start()
+    {
+        _actions.Add("hello", Hello);
+        _keywordRecognizer = new KeywordRecognizer(_actions.Keys.ToArray());
+        _keywordRecognizer.OnPhraseRecognized += OnPhraseRecognized;
+        _keywordRecognizer.Start();
+    }
+
+    private void Update()
+    {
+        Debug.Log("KeywordRecog.running: " + _keywordRecognizer.IsRunning);
+    }
+
+    private void OnDestroy()
+    {
+        _keywordRecognizer.Stop();
+        _keywordRecognizer.Dispose();
+    }
+}
