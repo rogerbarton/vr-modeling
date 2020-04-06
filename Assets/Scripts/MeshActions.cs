910d00a8ea2b1a0b4b632b59fba989e9fbcf474d
Roger Barton <rbarton@student.ethz.ch>
2020-04-06 20:22:50 +0200

    Start on simple UI generation based on wanted libigl functions
    Clean MeshManager of unnecessary functionality

---
+++ Assets/Scripts/MeshActions.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
+
+[Serializable]
+public class MeshAction
+{
+    public string name;
+    public Action action;
+    public string[] speechKeywords;
+    public int gestureId;
+
+    public MeshAction(string name, Action action, string[] speechKeywords, int gestureId)
+    {
+        this.name = name;
+        this.action = action;
+        this.speechKeywords = speechKeywords;
+        this.gestureId = gestureId;
+    }
+}
+
+/// <summary>
+/// Handles easy creation of operations to be done on a mesh and the user interaction (2D UI, speech, gestures)
+/// that comes with it.
+/// </summary>
+public class MeshActions : MonoBehaviour
+{
+    public List<MeshAction> actions;
+    public GameObject uiListItemPrefab;
+    public Transform uiListItemParent;
+    
+    private KeywordRecognizer keywordRecognizer;
+    
+    void Start()
+    {
+        // Add all actions
+        actions.Add(
+            new MeshAction(
+                "Smooth",
+                () =>
+                {
+                    Debug.Log("Execute Harmonic Smoothing");
+                },
+                new []{"smooth, harmonic, laplacian",},
+                0));
+        
+        // Setup UI
+        if (!uiListItemParent)
+            uiListItemParent = transform;
+        
+        // Create listitem foreach action
+        var allKeywords = new string[0];
+        for (int i = 0; i < actions.Count; i++)
+        {
+            // Parenting, layout, ui
+            var go = Instantiate(uiListItemPrefab, uiListItemParent);
+            var textField = go.GetComponentInChildren<TMP_Text>();
+            textField.text = actions[i].name;
+            
+            // setup callbacks/events
+            var button = go.GetComponent<Button>();
+            if(actions[i].action != null)
+                button.onClick.AddListener(new UnityAction(actions[i].action));
+            
+            // Setup speech keywords
+            allKeywords = allKeywords.Concat(actions[i].speechKeywords).ToArray();
+            
+            // TODO: Setup gesture recognition
+        }
+        
+        // Create one speech keywords recognizer for all actions
+        keywordRecognizer = new KeywordRecognizer(allKeywords);
+        keywordRecognizer.Start();
+    }
+
+    private void OnDestroy()
+    {
+        keywordRecognizer.Dispose();
+    }
+}
--- Assets/Scripts/MeshManager.cs
+++ Assets/Scripts/MeshManager.cs
@@ -1,26 +1,15 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
+using libigl;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.UIElements;
 
 public class MeshManager : MonoBehaviour
 {
     public GameObject[] meshPrefabs;
-    public List<GameObject> meshInstances;
-
-    public int activeMesh = -1;
-    public LayerMask raycastLayers;
+    public static LibiglMesh activeMesh;
     
     void Start()
     {
-        meshInstances = new List<GameObject>(transform.childCount);
-        for (int i = 0; i < transform.childCount; i++)
-            meshInstances.Add(transform.GetChild(i).gameObject);
-
-        activeMesh = -1;
+        activeMesh = Instantiate(0);
     }
 
     void Update()
@@ -33,20 +22,6 @@ public class MeshManager : MonoBehaviour
             Instantiate(2);
     }
 
-    private void OnMouseDown()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out var raycastHit, 10f,
-                raycastLayers))
-            {
-                var T = raycastHit.collider.transform;
-                if (T.parent.GetComponent<MeshManager>())
-                    activeMesh = T.GetSiblingIndex();
-            }
-        }
-    }
-
     #region Mesh Creation
     /// <summary>
     /// Instantiate a meshPrefab of a given index
@@ -54,24 +29,23 @@ public class MeshManager : MonoBehaviour
     /// <param name="prefabIndex">Index in meshPrefabs to be created</param>
     /// <param name="setAsActiveMesh">Set this mesh as the active one</param>
     /// <returns>Index in the meshInstances. Set to -1 if the index is invalid.</returns>
-    public int Instantiate(int prefabIndex, Vector3 pos = default, Quaternion rot = default, bool setAsActiveMesh = true)
+    public LibiglMesh Instantiate(int prefabIndex, Vector3 pos = default, Quaternion rot = default, bool setAsActiveMesh = true)
     {
         if (prefabIndex >= meshPrefabs.Length)
         {
             Debug.LogWarning("Invalid index for MeshManager.Instantiate(). " +
                              $"Given index {prefabIndex} and length {meshPrefabs.Length}");
-            return -1;
+            return null;
         }
         
         var go = Instantiate(meshPrefabs[prefabIndex], pos, rot);
         transform.parent = transform;
-        meshInstances.Add(go);
 
-        var instanceIndex = meshInstances.IndexOf(go);
-        if (setAsActiveMesh)
-            activeMesh = instanceIndex;
-        
-        return instanceIndex;
+        var mesh = go.GetComponent<LibiglMesh>();
+        if (!mesh)
+            mesh = go.AddComponent<LibiglMesh>();
+
+        return mesh;
     }
 
     public void InstantiateFromUI(int prefabIndex)
