8678bbabb3ab3f4776fd73558c62c9772a38f2d7
Roger Barton <rbarton@student.ethz.ch>
2020-05-01 21:44:07 +0200

    Add simple hand presence to load correct controller model

---
+++ Assets/Scripts/XRInteraction/HandPresence.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandPresence : MonoBehaviour
+{
+    public List<GameObject> controllerPrefabs;
+    public InputDeviceCharacteristics characteristics;
+    private InputDevice _rightHand;
+    private GameObject _controllerModel;
+    
+    private void Start()
+    {
+        var devices = new List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(characteristics, devices);
+
+        foreach (var item in devices)
+        {
+            Debug.Log(item.name + item.characteristics);
+        }
+
+        if (devices.Count > 0)
+        {
+            _rightHand = devices[0];
+
+            var prefab = controllerPrefabs.Find(go => _rightHand.name.StartsWith(go.name));
+            if (prefab)
+                _controllerModel = Instantiate(prefab, transform);
+            else
+            {
+                Debug.LogWarning("Could not find controller model, using default controller.");
+                _controllerModel = Instantiate(controllerPrefabs[0], transform);
+            }
+        }
+        else
+            Debug.LogWarning("No right hand controllers found.");
+    }
+
+    private void Update()
+    {
+        if(_rightHand.TryGetFeatureValue(CommonUsages.primaryButton, out var primaryBtnValue) && primaryBtnValue)
+            Debug.Log("Primary Button Pressed.");
+        
+        if(_rightHand.TryGetFeatureValue(CommonUsages.trigger, out var triggerValue) && triggerValue > 0)
+            Debug.Log($"Trigger value {triggerValue}");
+    }
+}
