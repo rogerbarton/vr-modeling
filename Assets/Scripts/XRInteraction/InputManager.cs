de84b633469e50dd3cee441e4dee86549d75e02e
Roger Barton <rbarton@student.ethz.ch>
2020-05-09 17:06:49 +0200

    Add left hand to InputManager

--- Assets/Scripts/XRInteraction/InputManager.cs
+++ Assets/Scripts/XRInteraction/InputManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class InputManager : MonoBehaviour
 {
@@ -9,13 +10,21 @@ public class InputManager : MonoBehaviour
 
     [SerializeField] private List<GameObject> controllerPrefabs;
 
+    // Left Hand
+    public InputDeviceCharacteristics leftHandChar =
+        InputDeviceCharacteristics.Controller | InputDeviceCharacteristics.Left;
+
+    [SerializeField] private Transform leftHandRig;
+
+    [NonSerialized] public InputDevice LeftHand;
+    
+    // Right Hand
     public InputDeviceCharacteristics rightHandChar =
         InputDeviceCharacteristics.Controller | InputDeviceCharacteristics.Right;
 
     [SerializeField] private Transform rightHandRig;
 
     [NonSerialized] public InputDevice RightHand;
-    [NonSerialized] public GameObject RightHandModel;
 
     private void Awake()
     {
@@ -31,28 +40,45 @@ public class InputManager : MonoBehaviour
 
     private void Start()
     {
+        InitializeController(leftHandChar, out LeftHand, leftHandRig);
+        InitializeController(rightHandChar, out RightHand, rightHandRig);
+    }
+
+    /// <summary>
+    /// Gets the XR InputDevice and sets the correct model to display.
+    /// </summary>
+    /// <param name="c"></param>
+    /// <param name="inputDevice"></param>
+    /// <param name="modelParent"></param>
+    private void InitializeController(InputDeviceCharacteristics c, out InputDevice inputDevice, Transform modelParent)
+    {
+        
         var devices = new List<InputDevice>();
-        InputDevices.GetDevicesWithCharacteristics(rightHandChar, devices);
+        InputDevices.GetDevicesWithCharacteristics(c, devices);
 
         foreach (var item in devices)
             Debug.Log(item.name + ": " + item.characteristics);
 
         if (devices.Count > 0)
         {
-            RightHand = devices[0];
+            inputDevice = devices[0];
 
-            var prefab = controllerPrefabs.Find(go => RightHand.name.StartsWith(go.name));
-            if (prefab)
-                RightHandModel = Instantiate(prefab, rightHandRig);
-            else
+            var deviceName = inputDevice.name;
+            var prefab = controllerPrefabs.Find(go => deviceName.StartsWith(go.name));
+            if (!prefab)
             {
                 //TODO: find correct names for Rift CV1 and Quest
-                Debug.LogWarning("Could not find controller model, using default controller.");
-                RightHandModel = Instantiate(controllerPrefabs[0], rightHandRig);
+                Debug.LogWarning($"Could not find controller model with name {deviceName}, using default controller.");
+                prefab = controllerPrefabs[0];
             }
+
+            modelParent.GetComponent<XRController>().modelPrefab = prefab.transform;
         }
         else
-            Debug.LogWarning("No right hand controller found.");
+        {
+            inputDevice = default;
+            Debug.LogWarning("No hand controller found with characteristic: " + c);
+        }
     }
 
     private void Update()
