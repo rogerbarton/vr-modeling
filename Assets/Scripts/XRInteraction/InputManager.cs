1226d1c6e44892e4c3083c4c5afe4b052f88465f
Roger Barton <rbarton@student.ethz.ch>
2020-05-10 13:22:37 +0200

    Add animated hand, can choose hands or controllers in InputManager, detect controllers

--- Assets/Scripts/XRInteraction/InputManager.cs
+++ Assets/Scripts/XRInteraction/InputManager.cs
@@ -1,34 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class InputManager : MonoBehaviour
 {
     public static InputManager get;
+
     /// <summary>
     /// Get the XR Rig Transform, to determine world positions
     /// </summary>
     public Transform XRRig;
 
-    [SerializeField] private List<GameObject> controllerPrefabs;
+    [Tooltip("Show animated hands or the controller? Needs to be set before the controller is detected")]
+    [SerializeField] private bool useHands = false;
+    [SerializeField] private GameObject leftHandPrefab = default;
+    [SerializeField] private GameObject rightHandPrefab = default;
+    [SerializeField] private List<GameObject> controllerPrefabs = default;
 
     // Left Hand
     public InputDeviceCharacteristics leftHandChar =
         InputDeviceCharacteristics.Controller | InputDeviceCharacteristics.Left;
 
-    [SerializeField] private Transform leftHandRig;
-
+    [SerializeField] private XRController leftHandRig = default;
     [NonSerialized] public InputDevice LeftHand;
-    
+
     // Right Hand
     public InputDeviceCharacteristics rightHandChar =
         InputDeviceCharacteristics.Controller | InputDeviceCharacteristics.Right;
 
-    [SerializeField] private Transform rightHandRig;
-
+    [SerializeField] private XRController rightHandRig = default;
     [NonSerialized] public InputDevice RightHand;
+
+    // Hand animation
+    private Animator _leftHandAnimator;
+    private Animator _rightHandAnimator;
+    private static readonly int TriggerAnimId = Animator.StringToHash("Trigger");
+    private static readonly int GripAnimId = Animator.StringToHash("Grip");
+
 
     private void Awake()
     {
@@ -40,37 +51,36 @@ public class InputManager : MonoBehaviour
         }
 
         get = this;
-        if(!XRRig)
+        if (!XRRig)
             XRRig = transform;
     }
 
     private void Start()
     {
-        InitializeController(leftHandChar, out LeftHand, leftHandRig);
-        InitializeController(rightHandChar, out RightHand, rightHandRig);
+        InitializeController(leftHandChar, out LeftHand, leftHandPrefab, leftHandRig, out _leftHandAnimator);
+        InitializeController(rightHandChar, out RightHand, rightHandPrefab, rightHandRig, out _rightHandAnimator);
     }
 
     /// <summary>
     /// Gets the XR InputDevice and sets the correct model to display.
     /// </summary>
-    /// <param name="c"></param>
-    /// <param name="inputDevice"></param>
-    /// <param name="modelParent"></param>
-    private void InitializeController(InputDeviceCharacteristics c, out InputDevice inputDevice, Transform modelParent)
+    private void InitializeController(InputDeviceCharacteristics c, out InputDevice inputDevice, GameObject handPrefab,
+        XRController modelParent, out Animator handAnimator)
     {
-        
         var devices = new List<InputDevice>();
         InputDevices.GetDevicesWithCharacteristics(c, devices);
 
         foreach (var item in devices)
             Debug.Log(item.name + ": " + item.characteristics);
 
+        handAnimator = null;
         if (devices.Count > 0)
         {
             inputDevice = devices[0];
 
             var deviceName = inputDevice.name;
-            var prefab = controllerPrefabs.Find(go => deviceName.StartsWith(go.name));
+            var prefab = useHands ? handPrefab : controllerPrefabs.Find(p => deviceName.StartsWith(p.name));
+
             if (!prefab)
             {
                 //TODO: find correct names for Rift CV1 and Quest
@@ -78,23 +88,53 @@ public class InputManager : MonoBehaviour
                 prefab = controllerPrefabs[0];
             }
 
-            modelParent.GetComponent<XRController>().modelPrefab = prefab.transform;
+            var go = Instantiate(prefab, modelParent.modelTransform);
+            if (useHands)
+                handAnimator = go.GetComponent<Animator>();
         }
         else
         {
             inputDevice = default;
-            Debug.LogWarning("No hand controller found with characteristic: " + c);
+            // Debug.LogWarning("No hand controller found with characteristic: " + c);
         }
     }
 
     private void Update()
     {
-        if (RightHand.TryGetFeatureValue(CommonUsages.primaryButton, out var primaryBtnValue) && primaryBtnValue)
-        {
-        }
+        if (!LeftHand.isValid)
+            InitializeController(leftHandChar, out LeftHand, leftHandPrefab, leftHandRig, out _leftHandAnimator);
 
-        if (RightHand.TryGetFeatureValue(CommonUsages.trigger, out var triggerValue) && triggerValue > 0)
+        if (!RightHand.isValid)
+            InitializeController(rightHandChar, out RightHand, rightHandPrefab, rightHandRig, out _rightHandAnimator);
+
+        if (useHands)
+            UpdateHandAnimators();
+    }
+
+    private void UpdateHandAnimators()
+    {
+        if (_leftHandAnimator)
         {
+            if (LeftHand.TryGetFeatureValue(CommonUsages.trigger, out var triggerLVal))
+                _leftHandAnimator.SetFloat(TriggerAnimId, triggerLVal);
+            if (LeftHand.TryGetFeatureValue(CommonUsages.grip, out var gripLVal))
+                _leftHandAnimator.SetFloat(GripAnimId, gripLVal);
         }
+        // else
+        // {
+        //     _leftHandAnimator = leftHandRig.modelTransform.GetComponentInChildren<Animator>();
+        // }
+
+        if (_rightHandAnimator)
+        {
+            if (RightHand.TryGetFeatureValue(CommonUsages.trigger, out var triggerRVal))
+                _rightHandAnimator.SetFloat(TriggerAnimId, triggerRVal);
+            if (RightHand.TryGetFeatureValue(CommonUsages.grip, out var gripRVal))
+                _rightHandAnimator.SetFloat(GripAnimId, gripRVal);
+        }
+        // else
+        // {
+        //     _rightHandAnimator = rightHandRig.modelTransform.GetComponentInChildren<Animator>();
+        // }
     }
 }
