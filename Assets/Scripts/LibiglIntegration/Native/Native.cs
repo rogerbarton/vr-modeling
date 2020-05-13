dbbe31a9f6ad6fcda5fb95d99293f3a78c7cd3f1
Roger Barton <rbarton@student.ethz.ch>
2020-05-13 13:37:11 +0200

    Remove optional parameters from c#

--- Assets/Scripts/LibiglIntegration/Native/Native.cs
+++ Assets/Scripts/LibiglIntegration/Native/Native.cs
@@ -85,9 +85,9 @@ namespace libigl
         public static extern unsafe void Harmonic(State* state);
 
         [DllImport(DllName)]
-        public static extern unsafe void SphereSelect(State* state, Vector3 position, float radiusSqr, int selectionId = 0);
+        public static extern unsafe void SphereSelect(State* state, Vector3 position, float radiusSqr, int selectionId);
         
         [DllImport(DllName)]
-        public static extern unsafe void SetColorBySelection(State* state, int selectionId = -1);
+        public static extern unsafe void SetColorBySelection(State* state, int selectionId);
     }
 }
