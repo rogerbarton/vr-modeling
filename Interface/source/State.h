97fe7a9633c1a6924d3de03029a4eafbf7fcbbc2
Roger Barton <rbarton@student.ethz.ch>
2020-05-26 13:53:21 +0200

    Default State values

--- Interface/source/State.h
+++ Interface/source/State.h
@@ -4,40 +4,40 @@
 
 struct InputState
 {
-	unsigned int ActiveTool;
+	unsigned int ActiveTool{0};
 
 	// Generic Input
-	float GripL = 0;
-	float GripR = 0;
-	Vector3 HandPosL = Vector3::Zero();
-	Vector3 HandPosR = Vector3::Zero();
+	float GripL{0};
+	float GripR{0};
+	Vector3 HandPosL{Vector3::Zero()};
+	Vector3 HandPosR{Vector3::Zero()};
 	// The previous position of the hand when the last transformation was made
-	Vector3 PrevTrafoHandPosL = Vector3::Zero();
-	Vector3 PrevTrafoHandPosR = Vector3::Zero();
+	Vector3 PrevTrafoHandPosL{Vector3::Zero()};
+	Vector3 PrevTrafoHandPosR{Vector3::Zero()};
 
 	// Transform
-	bool DoTransform = false;
-	bool PrimaryTransformHand = false; // True=R
-	bool SecondaryTransformHandActive = false;
+	bool DoTransform{false};
+	bool PrimaryTransformHand{false}; // True=R
+	bool SecondaryTransformHandActive{false};
 
 	// Select
-	int ActiveSelectionId = 0;
-	unsigned int ActiveSelectionMode = 0;
-	unsigned int SCount = 0;
-	unsigned int VisibleSelectionMask = -1; // All visible initially
+	int ActiveSelectionId{0};
+	unsigned int ActiveSelectionMode{0};
+	unsigned int SCount{0};
+	unsigned int VisibleSelectionMask{(unsigned int)-1}; // All visible initially
 
-	bool DoSelect = false;
-	Vector3 SelectPos = Vector3::Zero();
-	float SelectRadiusSqr = 0;
+	bool DoSelect{false};
+	Vector3 SelectPos{Vector3::Zero()};
+	float SelectRadiusSqr{0};
 	// A Mask of the selections that should be cleared
-	unsigned int DoClearSelection = 0;
+	unsigned int DoClearSelection{0};
 
 	// Harmonic
-	bool DoHarmonic = false;
+	bool DoHarmonic{false};
 };
 
 struct State {
-	unsigned int DirtyState = DirtyFlag::None;
+	unsigned int DirtyState{DirtyFlag::None};
 
 	Eigen::MatrixXf* V;
 	Eigen::MatrixXf* N;
@@ -45,16 +45,16 @@ struct State {
 	Eigen::MatrixXf* UV;
 	Eigen::MatrixXi* F;
 
-	int VSize = 0;
-	int FSize = 0;
+	int VSize{0};
+	int FSize{0};
 
 	// Latest InputState from PreExecute
-	InputState Input;
+	InputState Input{};
 
 	// Private C++ state
 	Eigen::VectorXi* S;
 	unsigned int* SSize; // uint[32], vertices selected per selection
-	unsigned int SSizeAll = 0; // Total vertices selected
+	unsigned int SSizeAll{0}; // Total vertices selected
 
 	explicit State(UMeshDataNative udata);
 
