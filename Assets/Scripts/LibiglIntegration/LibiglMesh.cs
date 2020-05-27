3dbd474d9ff20f447a9a8301b7454fe7f3cf50ae
Roger Barton <rbarton@student.ethz.ch>
2020-05-27 10:49:48 +0200

    Improve aborting thread

--- Assets/Scripts/LibiglIntegration/LibiglMesh.cs
+++ Assets/Scripts/LibiglIntegration/LibiglMesh.cs
@@ -111,8 +111,19 @@ namespace libigl
         {
             // Dispose all Native data (NativeArrays and anything created with 'new' in C++)
             // Note: may be called twice in the editor
-            _workerThread?.Interrupt();
-            _workerThread = null;
+            if(_workerThread != null)
+            {
+                try
+                {
+                    _workerThread.Abort();
+                    _workerThread.Join();
+                }
+                catch (Exception e)
+                {
+                    Debug.LogError(e.ToString());
+                }
+                _workerThread = null;
+            }
             DataRowMajor?.Dispose();
             DataRowMajor = null;
             Behaviour?.Dispose();
