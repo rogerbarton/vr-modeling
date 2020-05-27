0d0dd4a55d32bec6b18321b9d507c61e5f6f91b5
Roger Barton <rbarton@student.ethz.ch>
2020-05-27 17:45:54 +0200

    Basic custom shader node working

---
+++ Assets/Materials/Shaders/SelectionColor.hlsl
@@ -0,0 +1,24 @@
+﻿#ifndef SELECTIONCOLOR_HLSL
+#define SELECTIONCOLOR_HLSL
+
+float4 White, Black;
+float4 Red, Green, Blue, Orange, Pink;
+
+void SelectionColor_float (float uv1, out float4 Color)
+{
+    // Define consts
+    White = float4(1,1,1,1);
+    Black = float4(0,0,0,0);
+    Red = float4(0.7735849, 0.3280911, 0.280972, 1);
+    Green = float4(0.4173074, 0.7264151, 0.366634, 1);
+    Blue = float4(0.3019607, 0.4429668, 0.858823, 1);
+    Orange = float4(0.8784314, 0.5314119, 0.145098, 1);
+    Pink = float4(1, 0, 1, 1);
+    
+    uint maskId = (uint)uv1;
+   
+    
+    
+    Color = Pink;
+}
+#endif //SELECTIONCOLOR_HLSL
