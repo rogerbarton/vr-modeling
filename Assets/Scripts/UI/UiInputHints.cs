9b3ec702c853e1729960e8566cc1f80dc4717994
Roger Barton <rbarton@student.ethz.ch>
2020-06-09 10:43:36 +0200

    Input hints on controllers started
    prefab variant of touch controller done with ui panel
    labels that auto expand to fit children
    optional icon

---
+++ Assets/Scripts/UI/UiInputHints.cs
@@ -0,0 +1,14 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    public class UiInputHints : MonoBehaviour
+    {
+        public UiInputLabel trigger;
+        public UiInputLabel grip;
+        public UiInputLabel primaryBtn;
+        public UiInputLabel secondaryBtn;
+        public UiInputLabel primaryAxis;
+    }
+}
---
+++ Assets/Scripts/UI/UiInputLabel.cs
@@ -0,0 +1,13 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class UiInputLabel : MonoBehaviour
+    {
+        public Image background;
+        public Image icon;
+        public TMP_Text text;
+    }
+}
