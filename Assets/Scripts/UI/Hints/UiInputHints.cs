42f05c1ecd11e9528985b038cde22e17b0b49d2e
Roger Barton <rbarton@student.ethz.ch>
2020-06-29 13:45:04 +0200

    Move/rename files

---
+++ Assets/Scripts/UI/Hints/UiInputHints.cs
@@ -0,0 +1,197 @@
+using System;
+using Libigl;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Util;
+using XrInput;
+
+namespace UI
+{
+    public class UiInputHints : MonoBehaviour
+    {
+        public UiInputLabel title;
+        public UiInputLabel help;
+        public UiInputLabel trigger;
+        public UiInputLabel grip;
+        public UiInputLabel primaryBtn;
+        public UiInputLabel secondaryBtn;
+        public UiInputLabel primaryAxisX;
+        public UiInputLabel primaryAxisY;
+
+        [SerializeField] private UiInputHintsDataCollection collection = null;
+        private UiInputHintsData _currentData;
+
+        private LibiglBehaviour _activeBehaviour;
+
+        public void Initialize()
+        {
+            title.Initialize();
+            help.Initialize();
+            trigger.Initialize();
+            grip.Initialize();
+            primaryBtn.Initialize();
+            secondaryBtn.Initialize();
+            primaryAxisX.Initialize();
+            primaryAxisY.Initialize();
+            
+            _activeBehaviour = MeshManager.ActiveMesh.Behaviour; 
+            _activeBehaviour.OnActiveSelectionChanged += RepaintTriggerColor;
+            RepaintTriggerColor();
+
+            MeshManager.OnActiveMeshChanged += OnActiveMeshChanged;
+        }
+
+        public void SetData(UiInputHintsData data)
+        {
+            _currentData = data;
+            title.SetData(data.title);
+            help.SetData(data.help);
+            trigger.SetData(data.trigger);
+            grip.SetData(data.grip);
+            primaryBtn.SetData(data.primaryBtn);
+            secondaryBtn.SetData(data.secondaryBtn);
+            primaryAxisX.SetData(data.primaryAxisX);
+            primaryAxisY.SetData(data.primaryAxisY);
+        }
+
+        public void SetTooltip(string data, bool overrideExisting = false)
+        {
+            if(!_currentData.help.isActive || overrideExisting)
+                help.SetData(new UiInputLabelData{isOverride = true, isActive = true, text = data, icon = Icons.get.help}, false);
+        }
+
+        public void ClearTooltip()
+        {
+            help.ResetToData();
+        }
+        
+        /// <summary>
+        /// Display the message when hovering over this UI element.
+        /// Note: Must be attached to the gameObject with the button/toggle component, otherwise clicking may be blocked
+        /// </summary>
+        /// <param name="uiElement">GameObject with the Button/Toggle UI component</param>
+        /// <param name="msg">Message to display when hovering</param>
+        public static void AddTooltip(GameObject uiElement, string msg)
+        {
+            // Hover Tooltip
+            var onHoverStart = new EventTrigger.Entry {eventID = EventTriggerType.PointerEnter};
+            onHoverStart.callback.AddListener(_ =>
+            {
+                if(InputManager.get.HandHintsL)
+                    InputManager.get.HandHintsL.SetTooltip(msg, true);
+            });
+            
+            var onHoverEnd = new EventTrigger.Entry {eventID = EventTriggerType.PointerExit};
+            onHoverEnd.callback.AddListener(_ =>
+            {
+                if(InputManager.get.HandHintsL)
+                    InputManager.get.HandHintsL.ClearTooltip();
+            });
+            
+            var trigger = uiElement.gameObject.AddComponent<EventTrigger>();
+            trigger.triggers.Add(onHoverStart);
+            trigger.triggers.Add(onHoverEnd);
+        }
+        
+        public static void AddTooltip(GameObject uiElement, Func<string> msg)
+        {
+            // Hover Tooltip
+            var onHoverStart = new EventTrigger.Entry {eventID = EventTriggerType.PointerEnter};
+            onHoverStart.callback.AddListener(_ =>
+            {
+                if(InputManager.get.HandHintsL)
+                    InputManager.get.HandHintsL.SetTooltip(msg(), true);
+            });
+            
+            var onHoverEnd = new EventTrigger.Entry {eventID = EventTriggerType.PointerExit};
+            onHoverEnd.callback.AddListener(_ =>
+            {
+                if(InputManager.get.HandHintsL)
+                    InputManager.get.HandHintsL.ClearTooltip();
+            });
+            
+            var trigger = uiElement.gameObject.AddComponent<EventTrigger>();
+            trigger.triggers.Add(onHoverStart);
+            trigger.triggers.Add(onHoverEnd);
+        }
+        
+        #region Callbacks
+
+        /// <summary>
+        /// Refreshes the Input hints based on the <see cref="ActiveTool"/> and the sub state like <see cref="ToolTransformMode"/>
+        /// </summary>
+        public void Repaint()
+        {
+            SetData(collection.defaultHints);
+            switch (InputManager.State.ActiveTool)
+            {
+                case ToolType.Transform:
+                    SetData(collection.transform);
+                    switch (InputManager.State.ToolTransformMode)
+                    {
+                        case ToolTransformMode.Idle:
+                            SetData(collection.transformIdle);
+                            break;
+                        case ToolTransformMode.TransformingL:
+                            SetData(collection.transformTransformingL);
+                            break;
+                        case ToolTransformMode.TransformingR:
+                            SetData(collection.transformTransformingR);
+                            break;
+                        case ToolTransformMode.TransformingLr:
+                            SetData(collection.transformTransformingLr);
+                            break;
+                    }
+
+                    break;
+                case ToolType.Select:
+                    SetData(collection.select);
+                    switch (InputManager.State.ToolSelectMode)
+                    {
+                        case ToolSelectMode.Idle:
+                            SetData(collection.selectIdle);
+                            RepaintTriggerColor();
+                            break;
+                        case ToolSelectMode.Selecting:
+                            SetData(collection.selectSelecting);
+                            RepaintTriggerColor();
+                            break;
+                        case ToolSelectMode.TransformingL:
+                            SetData(collection.selectTransformL);
+                            break;
+                        case ToolSelectMode.TransformingR:
+                            SetData(collection.selectTransformR);
+                            break;
+                        case ToolSelectMode.TransformingLr:
+                            SetData(collection.selectTransformLr);
+                            break;
+                    }
+                    break;
+            }
+        }
+        
+        void OnActiveMeshChanged()
+        {
+            // Update the RepaintTriggerColor to only be called for the ActiveMesh
+            _activeBehaviour.OnActiveSelectionChanged -= RepaintTriggerColor;
+            _activeBehaviour = MeshManager.ActiveMesh.Behaviour;
+            _activeBehaviour.OnActiveSelectionChanged += RepaintTriggerColor;
+            RepaintTriggerColor();
+        }
+
+        private void RepaintTriggerColor()
+        {
+            if (InputManager.State.ActiveTool != ToolType.Select) return;
+            var selectionId = MeshManager.ActiveMesh.Behaviour.Input.ActiveSelectionId;
+            trigger.SetColor(Colors.Get(selectionId));
+        }
+
+        #endregion
+
+        private void OnDestroy()
+        {
+            _activeBehaviour.OnActiveSelectionChanged -= RepaintTriggerColor;
+            MeshManager.OnActiveMeshChanged -= OnActiveMeshChanged;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Hints/UiInputLabel.cs
@@ -0,0 +1,80 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    [System.Serializable]
+    public struct UiInputLabelData
+    {
+        public bool isOverride;
+        public bool isActive;
+        public Sprite icon;
+        public string text;
+    }
+    
+    public class UiInputLabel : MonoBehaviour
+    {
+        public Image background;
+        public Image icon;
+        public TMP_Text text;
+
+        private UiInputLabelData _data;
+        private Color _defaultColor = Color.white;
+
+        public void Initialize()
+        {
+            _defaultColor = background.color;
+        }
+
+        public void SetData(UiInputLabelData data, bool overwriteData = true)
+        {
+            if(!data.isOverride) return;
+            if(overwriteData) _data = data;
+            
+            gameObject.SetActive(data.isActive);
+            if (!data.isActive) return;
+            
+            icon.gameObject.SetActive(data.icon);
+            icon.sprite = data.icon;
+            text.text = data.text;
+                
+            background.color = _defaultColor;
+
+            // We need to refresh the layout explicitly
+            LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform) transform);
+        }
+
+        #region Directly Modify & Override
+
+        public void SetText(string data)
+        {
+            text.text = data;
+            LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform) transform);
+        }
+
+        public void SetColor(Color value)
+        {
+            background.color = value;
+        }
+
+        public void SetIcon(Sprite sprite)
+        {
+            var prev = icon.gameObject.activeSelf;
+            icon.gameObject.SetActive(sprite);
+            icon.sprite = sprite;
+            LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform) transform);
+        }
+
+        /// <summary>
+        /// Resets the label to the last written data, set when <see cref="SetData"/> overwriteData = true.
+        /// </summary>
+        public void ResetToData()
+        {
+            SetData(_data);
+            SetColor(_defaultColor);
+        }
+
+        #endregion
+    }
+}
