ed7d74388b5c32e829fd3d451f82b6896b35d096
Roger Barton <rbarton@student.ethz.ch>
2020-07-02 10:49:19 +0200

    fix setup of sphinx-csharp, add as dependency (!)

--- conf.py
+++ conf.py
@@ -56,7 +56,7 @@ if read_the_docs_build:
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [  'sphinx.ext.mathjax',
-                'sphinx-csharp',
+                'sphinx_csharp',
                 'breathe',
                 'm2r']
 
