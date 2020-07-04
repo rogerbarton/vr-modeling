0a9ec42a911118763114519f42825f1698eb43af
Roger Barton <rbarton@student.ethz.ch>
2020-07-04 16:14:19 +0200

    Fix open links in new tab/navbar
    used other answer, before navbar was broken

--- conf.py
+++ conf.py
@@ -50,15 +50,44 @@ if read_the_docs_build:
 
 # Open external links in new tab: https://stackoverflow.com/a/61058326/9295437
 from sphinx.writers.html import HTMLTranslator
-class CustomHTMLTranslator(HTMLTranslator):
-    # Open external links in new tab
-    def visit_reference(self, node):
-        if node.get('newtab') or not (node.get('target') or node.get('internal') or 'refuri' not in node):
-            node['target'] = '_blank'
-            super().visit_reference(node)
+from docutils import nodes
+from docutils.nodes import Element
+
+class PatchedHTMLTranslator(HTMLTranslator):
+
+    def visit_reference(self, node: Element) -> None:
+        atts = {'class': 'reference'}
+        if node.get('internal') or 'refuri' not in node:
+            atts['class'] += ' internal'
+        else:
+            atts['class'] += ' external'
+            # Customize behavior (open in new tab, secure linking site)
+            atts['target'] = '_blank'
+            atts['rel'] = 'noopener noreferrer'
+        if 'refuri' in node:
+            atts['href'] = node['refuri'] or '#'
+            if self.settings.cloak_email_addresses and atts['href'].startswith('mailto:'):
+                atts['href'] = self.cloak_mailto(atts['href'])
+                self.in_mailto = True
+        else:
+            assert 'refid' in node, \
+                'References must have "refuri" or "refid" attribute.'
+            atts['href'] = '#' + node['refid']
+        if not isinstance(node.parent, nodes.TextElement):
+            assert len(node) == 1 and isinstance(node[0], nodes.image)
+            atts['class'] += ' image-reference'
+        if 'reftitle' in node:
+            atts['title'] = node['reftitle']
+        if 'target' in node:
+            atts['target'] = node['target']
+        self.body.append(self.starttag(node, 'a', '', **atts))
+
+        if node.get('secnumber'):
+            self.body.append(('%s' + self.secnumber_suffix) %
+                             '.'.join(map(str, node['secnumber'])))
 
 def setup(app):
-    app.set_translator('html', CustomHTMLTranslator)
+    app.set_translator('html', PatchedHTMLTranslator)
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
