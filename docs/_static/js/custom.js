f965ab9489dbbd399f71b05a780934ee264edc50
Roger Barton <rbarton@student.ethz.ch>
2020-07-24 16:42:10 +0200

    css lowlight private headers in c# w jquery

--- docs/_static/js/custom.js
+++ docs/_static/js/custom.js
@@ -1,3 +1,4 @@
 $(document).ready(function () {
     $('a[href^="http://"], a[href^="https://"]').not('a[class*=internal]').attr('target', '_blank');
+    $("p.breathe-sectiondef-title.rubric:contains('Private')").css('background-color', '#93b2c5');
 });
